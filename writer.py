ea48accca819ce82c70dabc510dc37ee6426f673
Dmitrii Murashkin <murdm@ztm82.math.uni-bremen.de>
2020-02-27 13:52:32 +0100

    support several datatypes

--- writer.py
+++ writer.py
@@ -16,8 +16,25 @@ def write_data_geotiff(input_data, output_path, gdal_data, dec=1):
         bands = input_data.shape[2]
         data = input_data
 
+    datatype_mapping = {'uint8': gdal.GDT_Byte,
+                        'uint16': gdal.GDT_UInt16,
+                        'uint32': gdal.GDT_UInt32,
+                        'int8': gdal.GDT_Byte,
+                        'int16': gdal.GDT_Int16,
+                        'int32': gdal.GDT_Int32,
+                        'float32': gdal.GDT_Float32,
+                        'float64': gdal.GDT_Float64,
+                        'complex64': gdal.GDT_CFloat64,
+                        }
+
+    try:
+        datatype = datatype_mapping[input_data.dtype.name]
+    except:
+        print('Unsupported datatype {0} for the input array.'.format(input_data.dtype))
+        return False
+
     driver = gdal.GetDriverByName('GTiff')
-    out = driver.Create(output_path, X // dec + 1 if np.remainder(X, dec) else X // dec, Y // dec + 1 if np.remainder(Y, dec) else Y // dec, bands, gdal.GDT_Byte, options=['COMPRESS=DEFLATE'])
+    out = driver.Create(output_path, X // dec + 1 if np.remainder(X, dec) else X // dec, Y // dec + 1 if np.remainder(Y, dec) else Y // dec, bands, datatype, options=['COMPRESS=DEFLATE'])
     for gcp in gcps:
         gcp.GCPLine /= dec
         gcp.GCPPixel /= dec
