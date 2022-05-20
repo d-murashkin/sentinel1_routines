8f7b88f688e2bcd4ab774bb82a9d1af1938770e4
Dmitrii Murashkin <Dmitrii.Murashkin@dlr.de>
2022-05-20 19:03:09 +0200

    get scene from storage

--- download.py
+++ download.py
@@ -8,12 +8,10 @@ __author__ = 'Dmitrii Murashkin'
 __email__ = 'murashkin@uni-bremen.de'
 
 import os
+import shutil
 import subprocess
 import datetime
-import shutil
-import stat
 
-import pandas as pd
 
 from .scene_management import get_scene_folder
 from .scene_management import get_date_folder
@@ -24,73 +22,6 @@ def set_dir(dir_path):
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
     return dir_path
-
-
-def create_list_of_products(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat, start_date, end_date, login, password, lock_folder='', return_path=False):
-    """ Return list of products that fit the specified rectangle and sensing date.
-    """
-    """ Ensure that time variables are of the datetime.date type. """
-    if not ((type(start_date) == datetime.date) or (type(start_date) == datetime.datetime)) and ((type(end_date) == datetime.date) or (type(end_date) == datetime.date)):
-        print('start_date and end_date are expected to be of the datetime.date type.')
-        return False
-
-    list_of_products = []
-    page = 1
-    while True:
-        subprocess.call(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dhusget.sh') + ' -u {7} -p {8} -L {9} -m Sentinel-1 -c {0},{1}:{2},{3} -T GRD -F "*_GRDM_*" -S {5}T00:00:00.000Z -E {6}T00:00:00.000Z -l 100 -P {4}'.format(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat, page, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), login, password, lock_folder), shell=True)
-        try:
-            df = pd.read_csv('products-list.csv', header=None, names=['name', 'address'])
-        except:
-            break
-        if df.empty:
-            break
-        list_of_products.append(df)
-        page += 1
-    result = pd.concat(list_of_products)
-    if return_path:
-        return [{'name': name, 'address': address} for name, address in zip(result['name'].tolist(), result['address'].tolist())]
-    return result['name'].tolist()
-
-
-def download_products(fld, llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat, start_date, end_date, login, password, lock_folder='', n=2):
-    """ Deprecated.
-        Ensure that time variables are of the datetime.date type. """
-    if not ((type(start_date) == datetime.date) or (type(start_date) == datetime.datetime)) and ((type(end_date) == datetime.date) or (type(end_date) == datetime.date)):
-        print('start_date and end_date are expected to be of the datetime.date or the datetime.datetime type.')
-        return False
-    
-    if not lock_folder:
-        lock_folder = './dhusget_lock/'
-    print('lock folder: {0}'.format(lock_folder))
-    
-    dhusget = os.path.join(fld, 'dhusget.sh')
-    shutil.copyfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dhusget.sh'), dhusget)
-    st = os.stat(dhusget)
-    try:
-        os.chmod(dhusget, st.st_mode | stat.S_IEXEC)
-    except:
-        "If Marcus owns the files, it's fine. Otherwise there is a problem with chmod."
-        pass
-    current_fld = os.getcwd()
-    os.chdir(fld)
-    if os.path.exists(os.path.join(fld, 'PRODUCT')):
-        for item in os.listdir(os.path.join(fld, 'PRODUCT')):
-            pth = os.path.join(fld, 'PRODUCT', item)
-            if os.stat(pth).st_size == 0:
-                os.remove(pth)
-    page = 1
-    while True:
-        subprocess.call('./dhusget.sh -u {7} -p {8} -L {9} -m Sentinel-1 -c {0},{1}:{2},{3} -T GRD -F "*_GRDM_*" -S {5}T00:00:00.000Z -E {6}T00:00:00.000Z -l 100 -P {4} -n {10} -o product -D'.format(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat, page, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), login, password, lock_folder, n), shell=True)
-        try:
-            df = pd.read_csv('products-list.csv', header=None, names=['name', 'address'])
-        except:
-            break
-        if df.empty:
-            break
-        page += 1
-    os.chdir(current_fld)
-    print("Download complete.")
-    return True
 
 
 def download_single_scene(scene_name, root_folder=False, output_folder='./'):
@@ -111,17 +42,17 @@ def download_single_scene(scene_name, root_folder=False, output_folder='./'):
         if root_folder is True:
             try:
                 root_folder = os.environ['S1PATH']
-            except:
+            except Exception:
                 print('Could not read $S1PATH environment variable.')
                 return False
         download_path = get_scene_folder(scene_name, root_folder)
-    
+
     try:
         asf_credentials = os.environ['ASF_CREDENTIALS']
         with open(asf_credentials) as f:
             username = f.readline()[:-1]
             passwd = f.readline()[:-1]
-    except:
+    except Exception:
         print('No credentials provided.')
         return False
 
@@ -136,24 +67,24 @@ def download_single_day(date, root_folder=False, output_folder='./', extra_folde
     if not (type(date) is datetime.datetime) or (type(date) is datetime.date):
         print('input should be a datetime.datetime or datetime.date instance.')
         return False
-    
+
     if not root_folder:
         download_path = output_folder
     else:
         if root_folder is True:
             try:
                 root_folder = os.environ['S1PATH']
-            except:
+            except Exception:
                 print('Could not read $S1PATH environment variable.')
                 return False
         download_path = get_date_folder(date, root_folder, extra_folder=extra_folder)
-            
+
     try:
         asf_credentials = os.environ['ASF_CREDENTIALS']
         with open(asf_credentials) as f:
             username = f.readline()[:-1]
             passwd = f.readline()[:-1]
-    except:
+    except Exception:
         print('No credentials provided.')
         return False
 
@@ -171,6 +102,29 @@ def download_single_day(date, root_folder=False, output_folder='./', extra_folde
     subprocess.call('aria2c --http-auth-challenge=true --http-user={0} --http-passwd={1} --check-integrity=true --max-tries=0 --max-concurrent-downloads=3 "{2}"'.format(username, passwd, search_string), shell=True)
 
     os.chdir(cwd)
+    return True
+
+
+def get_scene_from_storage(scene_name, output_folder, root_folder=False, extra_folder=''):
+    if not root_folder:
+        try:
+            root_folder = os.environ['S1PATH']
+        except KeyError:
+            print('Specify root folder of the storage.')
+            return False
+    if not os.path.exists(root_folder):
+        print(f'Root folder {root_folder} does not exist.')
+        return False
+    if not os.path.exists(output_folder):
+        print(f'Output folder {output_folder} does not exist')
+        return False
+
+    scene_folder = get_scene_folder(scene_name, root_folder=root_folder, ensure_existence=False, extra_folder=extra_folder)
+
+    name = '_'.join(scene_name.split('_')[:-1])
+    scene_path = [os.path.join(scene_folder, item) for item in os.listdir(scene_folder) if name in item]
+    if scene_path:
+        shutil.copy(scene_path[0], os.path.join(output_folder, os.path.basename(scene_path[0])))
     return True
 
 
