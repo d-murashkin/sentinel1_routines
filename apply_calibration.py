320490a305b073734a2dba9ff3541e447592de03
Dmitrii Murashkin <Dmitrii.Murashkin@dlr.de>
2023-09-19 14:46:38 +0200

    fix borders, add normalization, keep ocean mask option

--- apply_calibration.py
+++ apply_calibration.py
@@ -12,16 +12,19 @@ from sentinel1_routines.scene_management import get_scene_folder
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='create a GeoTiff from a Sentinel-1 scene with applied calibration.')
-    parser.add_argument('-t', help='type of the output: RGB / grayscale / dB (default)')
+    parser.add_argument('-t', default='db', help='type of the output: RGB / grayscale / dB (default)')
     parser.add_argument('-i', required=True, help='input file (Sentinel-1 scene)')
     parser.add_argument('-o', default='', help='output folder')
-    parser.add_argument('-b', help='band (hh or hv) if grayscale type is chosen')
-    parser.add_argument('-f', help='apply speckle noise filter (bilateral fiter, 5 pixel size)', action='store_true')
-    parser.add_argument('-iac', help='apply incidence angle correction (for sea ice)', action='store_true')
+    parser.add_argument('-c', default='hh', help='channel, hh (default) or hv, if grayscale type is chosen')
+    parser.add_argument('-f', action='store_true', help='apply speckle noise filter (bilateral fiter, 5 pixel size)')
+    parser.add_argument('-iac', action='store_true', help='apply incidence angle correction (for sea ice)')
     parser.add_argument('-p', action='store_true', help='parallel=True')
-    parser.add_argument('--root_folder', default=False, help='path to sentinel-1 scene storage')
+    parser.add_argument('--root_folder', default=os.environ['S1PATH'], help='path to sentinel-1 scene storage')
     parser.add_argument('--rf_ef', default='', help='extra folder for get_scene_folder (most likely not required)')
     parser.add_argument('--scale_noise', action='store_true')
+    parser.add_argument('--save_oceanmask', action='store_true', help='save oceanmask as an extra layer (not applicable to "grayscale" output type)')
+    parser.add_argument('--save_incidence_angle', action='store_true', help='save incidence angles as an extra layer (only for "dB" output type)')
+    parser.add_argument('--clip_normalize', default=False, nargs=2, type=float, help='if provided, should be a two-value tuple, clip high and low values and normalize to the given range')
     args = parser.parse_args()
 
     if not os.path.exists(args.i):
@@ -36,26 +39,43 @@ if __name__ == "__main__":
         print('Scene is not found (wrong path?)')
         sys.exit()
 
-    filt = True if args.f else False
-    inc_angle_corr = True if args.iac else False
-    parallel = True if args.p else False
     output = os.path.join(args.o, os.path.splitext(os.path.basename(path_to_scene))[0] + '.tiff')
-    if not args.t:
-        tp = 'db'
-    else:
-        tp = args.t.lower()
 
     print('Processing...')
     t = time.time()
-    if tp == 'rgb':
-        rgb(path_to_scene, output, speckle_filter=filt, incidence_angle_correction=inc_angle_corr, parallel=parallel, correct_hv=False, scale_noise=args.scale_noise)
-    elif tp == 'grayscale':
-        if not args.b:
-            print('Specify band with -b option: hh or hv')
-            sys.exit()
-        grayscale(path_to_scene, output, band=args.b.lower(), speckle_filter=filt, incidence_angle_correction=inc_angle_corr, parallel=parallel, correct_hv=False, scale_noise=args.scale_noise)
-    elif tp == 'db':
-        calibrated(path_to_scene, output, speckle_filter=filt, incidence_angle_correction=inc_angle_corr, parallel=parallel, correct_hv=False)
-    else:
-        print('Unrecognised type "{0}". Possible options are "RGB", "grayscale", "dB".'.format(tp))
+    match args.t.lower():
+        case 'rgb':
+            rgb(path_to_scene,
+                output,
+                speckle_filter=args.f,
+                incidence_angle_correction=args.iac,
+                parallel=args.p,
+                correct_hv=False,
+                scale_noise=args.scale_noise,
+                save_oceanmask=args.save_oceanmask,
+                )
+        case 'grayscale':
+            grayscale(path_to_scene,
+                      output,
+                      band=args.c.lower(),
+                      speckle_filter=args.f,
+                      incidence_angle_correction=args.iac,
+                      parallel=args.p,
+                      correct_hv=False,
+                      scale_noise=args.scale_noise,
+                      )
+        case 'db':
+            calibrated(path_to_scene,
+                       output,
+                       speckle_filter=args.f,
+                       incidence_angle_correction=args.iac,
+                       parallel=args.p,
+                       correct_hv=False,
+                       scale_noise=args.scale_noise,
+                       save_oceanmask=args.save_oceanmask,
+                       save_incidence_angle=args.save_incidence_angle,
+                       clip_normalize=args.clip_normalize,
+                       )
+        case _:
+            print('Unrecognised type "{0}". Possible options are "RGB", "grayscale", "dB".'.format(args.t))
     print('... is done in {0} seconds.'.format(int(time.time() - t)))
--- to_geotiff.py
+++ to_geotiff.py
@@ -11,7 +11,7 @@ def grayscale(input_path, output_path, band='hh', speckle_filter=True, scale_noi
     """
     try:
         p = Sentinel1Product(input_path, scale_noise=scale_noise)
-        p.read_data(keep_calibration_data=False, crop_borders=False, **kwargs)
+        p.read_data(keep_calibration_data=False, crop_borders=True, **kwargs)
     except Exception:
         return False
     if band.lower() == 'hh':
@@ -32,12 +32,12 @@ def grayscale(input_path, output_path, band='hh', speckle_filter=True, scale_noi
     return True
 
 
-def rgb(input_path, output_path, speckle_filter=True, scale_noise=False, nodata_value=0, **kwargs):
+def rgb(input_path, output_path, speckle_filter=True, scale_noise=False, nodata_value=0, save_oceanmask=False, **kwargs):
     """ Create an RBG image from calibrated HH, HV and HV/HH bands of the specified Sentinel-1 product.
     """
     try:
         p = Sentinel1Product(input_path, scale_noise=scale_noise)
-        p.read_data(keep_calibration_data=False, crop_borders=False, **kwargs)
+        p.read_data(keep_calibration_data=False, crop_borders=True, **kwargs)
     except Exception:
         print('Error reading {0}'.format(input_path))
         return False
@@ -49,7 +49,7 @@ def rgb(input_path, output_path, speckle_filter=True, scale_noise=False, nodata_
     ratio[ratio < 0] = 0
     ratio[ratio > 1] = 1
     
-    img = np.stack([p.HH.data, p.HV.data, ratio], axis=2)
+    img = np.dstack([p.HH.data, p.HV.data, ratio])
 
     if speckle_filter:
         try:
@@ -59,34 +59,61 @@ def rgb(input_path, output_path, speckle_filter=True, scale_noise=False, nodata_
             print('Failed to apply speckle filter (bilateral filter from opencv).')
     img *= 250
     img = img.astype(np.uint8) + 1
+
+    if save_oceanmask:
+        p.mask_land()
+        img = np.dstack([img, ~p.landmask])
     write_data_geotiff(img, output_path, p.gdal_data, nodata_val=nodata_value)
     return True
 
 
-def calibrated(input_path, output_path, save_incidence_angle=False, band='both', **kwargs):
+def calibrated(input_path, output_path, scale_noise=False, speckle_filter=False, save_incidence_angle=False, channel='both', save_oceanmask=False, clip_normalize=False, **kwargs):
     """ Create a geotiff with calibrated data (in dB).
+        If clip_normalize consists of two values, the output is normalized to the range.
     """
     try:
-        p = Sentinel1Product(input_path)
-        p.read_data(keep_calibration_data=True, crop_borders=False, **kwargs)
+        p = Sentinel1Product(input_path, scale_noise=scale_noise)
+        print(kwargs)
+        p.read_data(keep_calibration_data=True, crop_borders=True, **kwargs)
     except Exception:
         print('Error reading {0}'.format(input_path))
         return False
+    
+    match channel.lower():
+        case 'both':
+            channels = [p.HH, p.HV]
+        case 'hh':
+            channels = [p.HH]
+        case 'hv':
+            channels = [p.HV]
+        case _:
+            print('Wrong channel type: {0}.'.format(channel))
+            print('Choose one of the following: both, hh, hv.')
+    
+    if clip_normalize:
+        [channel.clip_normalize(clip_normalize) for channel in channels]
 
-    if band.lower() == 'both':
-        bands = [p.HH.data, p.HV.data]
-    elif band.lower() == 'hh':
-        bands = [p.HH.data]
-    elif band.lower() == 'hv':
-        bands = [p.HV.data]
+    if speckle_filter:
+        try:
+            import cv2
+            data = [cv2.bilateralFilter(channel.data, 5, 15, 15) for channel in channels]
+        except Exception:
+            data = [channel.data for channel in channels]
+            print('Failed to apply speckle filter (bilateral filter from opencv).')
     else:
-        print('Wrong band type: {0}.'.format(band))
-        print('Choose one of the following: both, hh, hv.')
+        data = [channel.data for channel in channels]
+    
+    data = np.dstack(data)
+
     if save_incidence_angle:
         p.interpolate_incidence_angle()
-        bands.append(p.incidence_angle)
-    data = np.stack(bands, axis=2)
-    write_data_geotiff(data, output_path, p.gdal_data)
+        data = np.dstack([data, p.incidence_angle])
+
+    if save_oceanmask:
+        p.mask_land()
+        data = np.dstack([data, ~p.landmask])
+
+    write_data_geotiff(data, output_path, p.gdal_data, nodata_val=np.NaN)
     return True
 
 
